1f64542d08796e071948b8fe1bc281778f81066b
cpkopp <cameronpkopp@gmail.com>
2023-04-12 18:31:21 -1000

    set up service

--- photos/src/app/app.module.ts
+++ photos/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ShowPhotosComponent } from './show-photos/show-photos.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ShowPhotosComponent } from './show-photos/show-photos.component';
     ShowPhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
